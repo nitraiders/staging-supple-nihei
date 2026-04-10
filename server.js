9d7d439b8e5e57f0cec208f899f7241e9921cd50
nitraiders <nitraiders2106@gmail.com>
2026-04-10 19:02:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// public フォルダを静的ファイルとして提供
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 全てのルートを index.html に向ける（SPA構成への配慮）
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use((req, res) => {
+      res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
 });
